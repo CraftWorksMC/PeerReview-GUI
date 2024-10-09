7bec757fda689cae8c3e3e23d98566ce91174b83
CraftWorks <github@craftworks.top>
2024-10-09 18:04:43 +0200

    Encrypt login info

--- composeApp/src/desktopMain/kotlin/com/craftworks/peerreview/SettingsManager.kt
+++ composeApp/src/desktopMain/kotlin/com/craftworks/peerreview/SettingsManager.kt
@@ -1,6 +1,10 @@
 package com.craftworks.peerreview
 
 import java.io.File
+import java.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 
 // Save string data to disk, in OS specific directories
@@ -17,8 +21,8 @@ fun saveData(fileName: String, data: String) {
     if (!file.parentFile.exists()) {
         file.parentFile.mkdirs()
     }
-    if(!file.exists()) file.createNewFile()
-    file.writeText(data) // Write string to file
+    if (!file.exists()) file.createNewFile()
+    file.writeText(encrypt(data)) // Write string to file
 }
 
 // Load string data to disk, in OS specific directories
@@ -34,8 +38,29 @@ fun loadData(fileName: String): String? {
 
     val file = File("$homeDir/PeerReviewData/$fileName")
     return if (file.exists()) {
-        file.readText() // Read string from file
+        decrypt(file.readText()) // Read string from file
     } else {
         null
     }
+}
+
+private val KEY = "rQ7bjJd0fjGvd1ToB3rRg4A9zTzJgkUE".toByteArray()
+private val IV = "f5jKZkdfRi4guH1c".toByteArray()
+
+private fun encrypt(plainText: String): String {
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+    val secretKey = SecretKeySpec(KEY, "AES")
+    val ivSpec = IvParameterSpec(IV)
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec)
+    val encrypted = cipher.doFinal(plainText.toByteArray())
+    return Base64.getEncoder().encodeToString(encrypted)
+}
+
+private fun decrypt(cipherText: String): String {
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+    val secretKey = SecretKeySpec(KEY, "AES")
+    val ivSpec = IvParameterSpec(IV)
+    cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec)
+    val decrypted = cipher.doFinal(Base64.getDecoder().decode(cipherText))
+    return String(decrypted)
 }
